001d81c64a1bb9eec66f364c275eea20130b2aa0
omaralbalolly <albalollyomar@gmail.com>
2022-07-26 09:56:34 +0300

    Define the staticfiles storage

--- django_elearning/settings.py
+++ django_elearning/settings.py
@@ -154,6 +154,6 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 import django_heroku
 django_heroku.settings(locals())
