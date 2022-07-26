31ae1f26c48af9e2862903d4bab074e8fc9ae89b
omaralbalolly <albalollyomar@gmail.com>
2022-07-26 13:56:01 +0300

    Fixed settings files errors

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_elearning.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_elearning.local_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
